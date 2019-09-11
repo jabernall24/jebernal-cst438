7d4c236e40f9b5c8c04e0c9cf9d447abaf944ac2
jabernall24 <jabernall24@gmail.com>
2019-09-10 18:43:47 -0700

    idk

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('testApp app is running!');
+    expect(page.getTitleText()).toEqual('hw2 app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testApp'`, () => {
+  it(`should have as title 'hw2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testApp');
+    expect(app.title).toEqual('hw2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('testApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('hw2 app is running!');
   });
 });
