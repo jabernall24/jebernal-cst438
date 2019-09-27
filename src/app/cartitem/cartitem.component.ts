a511e83c5c69fba13c05bf1c212def410a8aaba2
jabernall24 <jabernall24@gmail.com>
2019-09-27 16:56:03 -0700

    sprint 2 finished

---
+++ src/app/cartitem/cartitem.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cartitem',
+  templateUrl: './cartitem.component.html',
+  styleUrls: ['./cartitem.component.scss']
+})
+export class CartitemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  removeProduct() {
+    console.log("product removed");
+  }
+
+}
--- src/app/checkout/checkout.component.spec.ts
+++ src/app/checkout/checkout.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { CheckoutComponent } from './checkout.component';
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+describe('CheckoutComponent', () => {
+  let component: CheckoutComponent;
+  let fixture: ComponentFixture<CheckoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ CheckoutComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(CheckoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
